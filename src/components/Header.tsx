690b9d730ff7c41c3d8b547c1098c9def6a651c4
decembaek <tmdrbpp123@gmail.com>
2023-09-07 01:08:06 +0900

    main grid

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,15 @@ import {
   IconButton,
   Button,
   useDisclosure,
+  useColorMode,
+  useColorModeValue,
+  LightMode,
 } from '@chakra-ui/react';
-import { FaAirbnb, FaMoon } from 'react-icons/fa';
+import { FaAirbnb, FaMoon, FaSun } from 'react-icons/fa';
 import LoginModal from './LoginModal';
 import SignUpModal from './SignUpModal';
+
+// LightMode, DarkMode 태그 만들면 강제로 사용
 
 export default function Headers() {
   const {
@@ -20,26 +25,35 @@ export default function Headers() {
     onClose: onSignUpClose,
     onOpen: onSignUpOpen,
   } = useDisclosure();
+  const { colorMode, toggleColorMode } = useColorMode();
+  // 컬러모드에 따라 true, false
+  const logoColor = useColorModeValue('red.500', 'red.200');
+  // Icon으로 쓸때는 무조건 대문자로 시작
+  const Icon = useColorModeValue(FaMoon, FaSun);
   return (
     <HStack
       justifyContent={'space-between'}
       py={5}
-      px={10}
+      px={40}
       borderBottomWidth={1}
     >
-      <Box color={'red.500'}>
+      <Box color={logoColor}>
         <FaAirbnb size={38} />
       </Box>
       <HStack>
         <IconButton
+          onClick={toggleColorMode}
           variant={'ghost'}
           aria-label="Toggle dark mode"
-          icon={<FaMoon />}
+          // icon={colorMode === 'light' ? <FaMoon /> : <FaSun />}
+          icon={<Icon />}
         />
         <Button onClick={onLoginOpen}>Log in</Button>
-        <Button onClick={onSignUpOpen} colorScheme="red">
-          Sign up
-        </Button>
+        <LightMode>
+          <Button onClick={onSignUpOpen} colorScheme="red">
+            Sign up
+          </Button>
+        </LightMode>
       </HStack>
       <LoginModal isOpen={isLoginOpen} onClose={onLoginClose} />
       <SignUpModal isOpen={isSignUpOpen} onClose={onSignUpClose} />
--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
-import { RouterProvider } from "react-router-dom";
-import router from "./router";
-import { DevSupport } from "@react-buddy/ide-toolbox";
-import { ComponentPreviews, useInitial } from "./dev";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
+// import { DevSupport } from '@react-buddy/ide-toolbox';
+// import { ComponentPreviews, useInitial } from './dev';
+import theme from './theme';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <DevSupport
-        ComponentPreviews={ComponentPreviews}
-        useInitialHook={useInitial}
-      >
-        <RouterProvider router={router} />
-      </DevSupport>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <RouterProvider router={router} />
     </ChakraProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
